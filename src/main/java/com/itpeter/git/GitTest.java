bf4074aa2968ccace3d29e72ffc54fbe4fcfa590
peter <peter@itpeter.com>
2023-03-12 00:10:12 +0800

    second commit

--- src/main/java/com/itpeter/git/GitTest.java
+++ src/main/java/com/itpeter/git/GitTest.java
@@ -8,5 +8,6 @@ package com.itpeter.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("git hello");
+        System.out.println("git hello2");
     }
 }
