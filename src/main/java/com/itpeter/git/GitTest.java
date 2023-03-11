855c6f2e90fe82cc84ded14545c5c9459d49d408
peter <peter@itpeter.com>
2023-03-12 00:11:52 +0800

    third commit

--- src/main/java/com/itpeter/git/GitTest.java
+++ src/main/java/com/itpeter/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("git hello");
         System.out.println("git hello2");
+        System.out.println("git hello3");
     }
 }
