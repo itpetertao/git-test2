7ca11fad1a1939a33a09775a78bb1596d9108928
peter <peter@itpeter.com>
2023-03-12 00:07:44 +0800

    first commit

---
+++ src/main/java/com/itpeter/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.itpeter.git;
+
+/**
+ * @author 黄榕涛
+ * @version 1.0
+ * @create 2023/3/11 23:52
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("git hello");
+    }
+}
